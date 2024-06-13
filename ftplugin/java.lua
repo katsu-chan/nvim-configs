1474f4c374a502bf37e9e7eb2171fae462e5e2e4
Eveeee <44003480+katsu-chan@users.noreply.github.com>
2024-06-13 05:10:10 +0300

    Deleted everything

--- ftplugin/java.lua
+++
@@ -1,8 +0,0 @@
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-local config = {
-	capabilities = capabilities,
-    cmd = {'/usr/bin/jdtls'},
-    root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
-}
-require('jdtls').start_or_attach(config)
--- init.lua
+++
@@ -1,7 +0,0 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-vim.opt.rtp:prepend(lazypath)
-
-require("config")
-
-require("lazy").setup("plugins")
-
